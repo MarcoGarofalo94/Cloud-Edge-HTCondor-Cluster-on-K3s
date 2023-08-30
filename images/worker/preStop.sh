a5e386d9bbaf989d4cb9e82f487b1123433933bd
MarcoGarofalo94 <garofalomarco1994@libero.it>
2023-08-30 14:41:58 +0200

    add: preStop script

---
+++ images/worker/preStop.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Notify central manager of this worker going down so that it can be removed from the pool
+condor_off -peaceful
+
+# Specify the username prefix
+USERNAME_PREFIX="slot"
+
+# Get the User IDs (UIDs) matching the username prefix
+USER_IDS=$(grep "^$USERNAME_PREFIX" /etc/passwd | cut -d':' -f3)
+
+# Loop through each user ID and find their processes
+for USER_ID in $USER_IDS; do
+    # Find PIDs of processes created by the user
+    PIDS=$(ps -u $USER_ID -o pid=)
+
+    if [ -n "$PIDS" ]; then
+        echo "User with UID $USER_ID created processes with PIDs: $PIDS"
+
+        # Wait for processes to finish
+        while true; do
+            all_processes_done=true
+            for PID in $PIDS; do
+                if ps -p $PID > /dev/null; then
+                    all_processes_done=false
+                    break
+                fi
+            done
+
+            if $all_processes_done; then
+                break
+            fi
+
+            sleep 5  # Adjust the sleep duration as needed
+        done
+
+        echo "All processes for user with UID $USER_ID have finished."
+    else
+        echo "User with UID $USER_ID has no active processes."
+    fi
+done
